b01d6d36632e6fdac0dfa1e8cb549cc15fc91a48
Matheus Pinheiro Dos Santos <ra119920@yes.lab.ic.unicamp.br>
2016-03-09 16:14:05 -0300

    files for different tokens

---
+++ tokens/lyalex_relationals.py
@@ -0,0 +1,63 @@
+import ply.lex as lex
+import sys
+
+# List of token name.
+
+tokens = (
+
+# Reserved Words
+'ARRAY', 'BY', 'CHARS', 'DCL', 'DO', 'DOWN', 'ELSE', 'ELSIF', 'END', 'EXIT', 'FI', 'FOR', 'IF', 'IN', 'LOC', 'TYPE', 'OD', 'PROC', 'REF', 'RESULT', 'RETURN', 'RETURNS', 'SYN', 'THEN', 'TO', 'WHILE',
+
+# Predefined words
+
+'BOOL', 'CHAR', 'FALSE', 'INT', 'LENGTH', 'LOWER', 'NULL', 'NUM','PRED', 'PRINT', 'READ', 'SUCC', 'TRUE', 'UPPER',
+
+# Defined token
+
+'COMMA', 'SMC', 'COLON', 'LPAREN', 'RPAREN', 'EQUAL', 'ISEQUAL', 'PEQUAL', 'TEQUAL', 'MEQUAL', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'GTHAN', 'LTHAN', 'GETHAN', 'LETHAN', 'NUMBER'
+
+)
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# open file
+with open(sys.argv[1], 'r') as myfile:
+    data=myfile.read()
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
