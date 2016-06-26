5f0315cc834bf75d79dd2de43e390e8081f5a81c
Matheus <matheuspinheiro.santos@gmail.com>
2016-06-26 17:23:12 -0300

    lvm

--- projeto2/lvm.py
+++ projeto2/lvm.py
@@ -1,1 +1,17 @@
 
+class LyaVirtualMachine(object):
+	
+	def __init__(self, program):
+		self.pc = 0
+		self.sp = 0
+		self.program = program
+		
+		self.M = []
+		self.D = []
+
+	def execute(self):
+		for instruction in self.program:
+			if type(instruction) is tuple:
+				inst = instruction[0]
+				print(inst)
+				print("")
--- projeto2/testLvm.py
+++ projeto2/testLvm.py
@@ -2,17 +2,13 @@ import sys
 from nodeVisitor import Visitor
 from lyaParser import LyaParser
 from genCode import CodeGenerator
+from lvm import LyaVirtualMachine
 
 p = LyaParser()
 if(len(sys.argv) == 2):
 	tree = p.parse_file(sys.argv[1], 0)
 else:
 	tree = p.parse_file("examples/mTest.lya", 0)
-
-print("")
-print("--- TREE ---")
-print("")
-tree.show()
 
 print("")
 print("--- SEMANTIC ---")
@@ -27,3 +23,8 @@ codeGen = CodeGenerator()
 codeGen.visit(tree)
 codeGen.printInstructions()
 
+print("")
+print("--- LVM ---")
+print("")
+lvm = LyaVirtualMachine(codeGen.program)
+lvm.execute()
