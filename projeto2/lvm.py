c54ff4a38b5f4dc71267aafa488e2c4520b18680
Gustavo Rodrigues Basso <ra105046@tramp.lab.ic.unicamp.br>
2016-06-26 19:24:14 -0300

    LVM v1.3

--- projeto2/lvm.py
+++ projeto2/lvm.py
@@ -8,6 +8,7 @@ class LyaVirtualMachine(object):
 		self.program = []
 		self.M = []
 		self.D = []
+		self.H = []
 		self.labelsMap = {}
 
 	def initExecution(self):
@@ -16,6 +17,7 @@ class LyaVirtualMachine(object):
 		self.program = []
 		self.M = [None] * 1000
 		self.D = [None] * 100
+		self.H = [None] * 100
 		self.labelsMap = {}
 
 	def getCurrentInstruction(self):
@@ -141,6 +143,47 @@ class LyaVirtualMachine(object):
 		self.sp -= 1
 		self.pc += 1
 
+#	NEG: M[sp]= -M[sp]
+#	(’neg’)
+
+	def execute_neg(self, instruction):
+		self.M[self.sp] = - self.M[self.sp]
+		self.pc += 1
+
+#	AND: M[sp-1]=M[sp-1] and M[sp];  sp=sp-1
+#	(’and’)
+
+	def execute_and(self, instruction):
+		self.M[self.sp-1] = self.M[self.sp-1] and self.M[self.sp]
+		self.sp -= 1
+		self.pc += 1
+
+#	LOR: M[sp-1]=M[sp-1] or M[sp];  sp=sp-1
+#	(’lor’)
+
+	def execute_and(self, instruction):
+		self.M[self.sp-1] = self.M[self.sp-1] or self.M[self.sp]
+		self.sp -= 1
+		self.pc += 1
+
+#	NOT:  M[sp]= not M[sp]
+#	(’not’)
+
+	def execute_not(self, instruction):
+		self.M[self.sp] = not self.M[self.sp]
+		self.pc += 1
+
+#	LES:  M[sp-1]=M[sp-1] < M[sp];  sp=sp-1
+#	(’les’)
+
+	def execute_les(self, instruction):
+		if self.M[self.sp-1] < self.M[self.sp]:
+			 self.M[self.sp-1] = 1
+		else:
+			self.M[self.sp -1] = 0
+		self.sp -= 1
+		self.pc += 1
+
 #	LEQ: M[sp-1]=M[sp-1] <= M[sp];  sp=sp-1
 #	(’leq’) 
 
@@ -150,6 +193,50 @@ class LyaVirtualMachine(object):
 		else:
 			self.M[self.sp-1] = 0
 			
+		self.sp -= 1
+		self.pc += 1
+
+#	GRT: M[sp-1]=M[sp-1] > M[sp];  sp=sp-1
+#	(’grt’)
+
+	def execute_grt(self, instruction):
+		if self.M[self.sp-1] > self.M[self.sp] :
+			self.M[self.sp-1] = 1
+		else:
+			self.M[self.sp -1] = 0
+		self.sp -= 1
+		self.pc += 1
+		
+#	GRE: M[sp-1]=M[sp-1] >= M[sp];  sp=sp-1
+#	(’gre’)
+
+	def execute_gre(self, instruction):
+		if self.M[self.sp-1] >= self.M[self.sp] :
+			self.M[self.sp-1] = 1
+		else:
+			self.M[self.sp -1] = 0
+		self.sp -= 1
+		self.pc += 1
+
+#	EQU: M[sp-1]=M[sp-1] == M[sp];  sp=sp-1
+#	(’equ’)
+
+	def execute_equ(self, instruction):
+		if self.M[self.sp-1] == self.M[self.sp] :
+			self.M[self.sp-1] = 1
+		else:
+			self.M[self.sp -1] = 0
+		self.sp -= 1
+		self.pc += 1
+
+#	NEQ: M[sp-1]=M[sp-1] != M[sp];  sp=sp-1
+#	(’neq’)
+
+	def execute_neq(self, instruction):
+		if self.M[self.sp-1] != self.M[self.sp] :
+			self.M[self.sp-1] = 1
+		else:
+			self.M[self.sp -1] = 0
 		self.sp -= 1
 		self.pc += 1
 
@@ -185,6 +272,114 @@ class LyaVirtualMachine(object):
 		self.sp -= instruction[1]
 		self.pc += 1
 
+#	CFU:  sp=sp+1; M[sp]=pc+1; pc=p
+#	(’cfu’, p)
+
+	def execute_cfu(self, instruction):
+		self.sp += 1
+		self.M[self.sp] = self.pc+1
+		self.pc += self.labelsMap[instruction[1]]
+
+
+#	ENF: sp=sp+1; M[sp]=D[k]; D[k]=sp+1
+#	(’enf’, k)
+
+	def execute_enf(self, instruction):
+		self.sp += 1
+		self.M[self.sp] = self.D[instruction[1]]
+		self.D[instruction[1]] = self.sp + 1
+		self.pc += 1
+
+#	RET: D[k]=M[sp]; pc=M[sp-1]; sp=sp-(n+2)
+#	(’ret’, k, n)
+
+	def execute_ret(self, instruction):
+		self.D[instruction[1]] = self.M[self.sp]
+		self.pc = self.M[self.sp-1]
+		self.sp -= instruction[2] + 2
+
+#	IDX: M[sp-1]=M[sp-1] + M[sp] * k; sp=sp-1;
+#	(’idx’, k)
+
+	def execute_idx(self, instruction):
+		self.M[self.sp-1] = self.M[self.sp-1] + self.M[self.sp] * instruction[1]
+		self.sp -= 1
+		self.pc += 1
+
+#	GRC: M[sp]=M[M[sp]]
+#	(’grc’)
+
+	def execute_grc(self, instruction):
+		self.M[self.sp] = self.M[ self.M[self.sp] ]
+		self.pc += 1
+
+#	LMV: t=M[sp]; M[sp:sp+k]=M[t:t+k]; sp += (k-1)
+#	(’lmv’, k)
+
+	def execute_lmv(self, instruction):
+		t = self.M[self.sp]
+		for i=0 to instruction[1]:
+			self.M[self.sp + i] = self.M[t + i]
+		self.sp += instruction[1] - 1 
+		self.pc += 1
+
+#	SMV: t = M[sp-k]; M[t:t+k] =M[sp-k+1:sp+1]; sp -= (k+1);
+#	(’smv’, k)
+
+	def execute_smv(self, instruction):
+		t = self.M[self.sp-instruction[1]]
+		for i=0 to instruction[1]:
+			self.M[t + i] = self.M[self.sp - instruction[1] + 1 +i ]
+		self.sp -= instruction[1] + 1
+		self.pc += 1
+
+#	SMR: t1 = M[sp-1]; t2 = M[sp]; M[t1:t1+k] = M[t2:t2+k]; sp -= 1;
+#	(’smr’, k)
+
+	def execute_smr(self, instruction):
+		t1 = self.M[self.sp-1]
+		t2 = self.M[self.sp]
+		
+		for i=0 to instruction[1]:
+			self.M[t1 + i] = self.M[t2 + i]
+		self.sp -= 1
+		self.pc += 1
+
+#	STS: adr=M[sp]; M[adr]=len(H[k]); for c in H[k]: adr=adr+1; M[adr]=c; sp=sp-1
+#	(’sts’, k)
+
+	def execute_sts(self, instruction):
+		adr = self.M[self.sp]
+		self.M[adr] =  len ( self.H[instruction[1] )
+		
+		for c in self.H[instruction[1]]:
+			adr += 1
+		
+		self.M[adr] = c
+		self.sp -= 1
+		self.pc += 1
+
+#	RDV: sp=sp+1;  M[sp]=input()
+#	(’rdv’)
+
+	def execute_rdv(self, instruction):
+		self.sp += 1
+		self.M[self.sp] = input()
+		self.pc += 1
+
+#	RDS: str=input(); adr=M[sp]; M[adr] = len(str); for k in str: adr=adr+1 M[adr]=k; sp=sp-1;
+#	(’rds’)
+
+	def execute_rds(self, instruction):
+		str = input() 
+		adr = self.M[self.sp]
+		self.M[adr] = len (str)
+		for k in str:
+			adr += 1
+			self.M[adr] = k
+		self.sp -= 1
+		self.pc += 1
+
 #	PRV: print(M[sp]); sp=sp-1
 #	(’prv’)
 
@@ -193,8 +388,45 @@ class LyaVirtualMachine(object):
 		self.sp -= 1
 		self.pc += 1
 
+#	PRT: print(M[sp-k+1:sp+1]); sp-=(k-1)
+#	('prt', k)
+
+	def execute_prt(self, instruction):
+		for i=instruction[1] to 0:
+			print(self.M[self.sp - i + 1])
+		self.sp -= instruction[1] - 1
+		self.pc += 1
+
+#	PRC: print(H(i),end="")
+#	(’prc’, i)
+
+	def execute_prc(self, instruction):
+		print(self.H[instruction[1]], end="")
+		self.pc += 1
+
+#	PRS: adr = M[sp]; len = M[adr]; for i in range(0,len): adr = adr + 1 print(M(adr),end=""); sp=sp-1;
+#	(’prs’)
+
+	def execute_prs(self, instruction):
+		adr = self.M[self.sp]
+		b = self.M[adr]
+		for i in range(0, b):
+			adr += 1
+			print(self.M[adr], end="")
+		self.sp -= 1
+		self.pc += 1
+
 #	LBL:
 #	(’lbl’, i)
 
 	def execute_lbl(self, instruction):
 		self.pc += 1
+		
+#	END:
+#	(’end’)
+
+	def execute_end(self, instruction):
+		pass
+
+
+
